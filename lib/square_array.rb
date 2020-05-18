f099ddfb8f9ce14485e63e640f65f7d728b53d65
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-18 00:21:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  i = 0
+  sq_array = []
+  while (i + 1) < array.length do
+    sq_array[i] = array[i] ** 2
+    p "sq_array[#{i}] = #{sq_array[i]}"
+    p "array[#{i}] = #{array[i]}"
+  end
 end
+
+square_array([1,2,3,4])
